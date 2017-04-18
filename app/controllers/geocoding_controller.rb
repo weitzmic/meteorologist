67244bd2c92c23d259a87e740bdb150bccf0202b
Michael Weitz <mweitz0@chicagobooth.edu>
2017-04-18 12:36:37 -0500

    meteorologist_update

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -14,12 +14,13 @@ class GeocodingController < ApplicationController
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}"
+    parsed_data = JSON.parse(open(url).read)
+    latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-
-
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    @latitude = latitude
+    @longitude = longitude
 
     render("geocoding/street_to_coords.html.erb")
   end
